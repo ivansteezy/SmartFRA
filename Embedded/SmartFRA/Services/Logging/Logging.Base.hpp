8b10a58ef35bcd05b3b475fae9ac20a30c9cbaee
Syro <network.9961@gmail.com>
2022-09-14 22:19:15 -0500

    working project structure

---
+++ Embedded/SmartFRA/Services/Logging/Logging.Base.hpp
@@ -0,0 +1,10 @@
+#ifndef LOGGING_BASE_HPP
+#define LOGGING_BASE_HPP
+#include <iostream>
+
+void MessageLogging()
+{
+    std::cout << "Hello from loggging \n";
+}
+
+#endif // LOGGING_BASE_HPP
---
+++ Embedded/SmartFRA/Services/Networking/Networking.Base.hpp
@@ -0,0 +1,11 @@
+#ifndef NETWORKING_BASE_HPP
+#define NETWORKING_BASE_HPP
+
+#include <iostream>
+
+void MessageNetworking()
+{
+    std::cout << "Hello from networking\n";
+}
+
+#endif // NETWORKING_BASE_HPP
