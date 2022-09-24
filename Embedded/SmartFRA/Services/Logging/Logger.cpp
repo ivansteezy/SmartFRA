2f0834fd1d1ee09cdb44438b9638ad4d10c6427c
Syro <network.9961@gmail.com>
2022-09-23 20:18:49 -0500

    fixing error file formating

--- Embedded/SmartFRA/Services/Logging/Logger.cpp
+++ Embedded/SmartFRA/Services/Logging/Logger.cpp
@@ -42,8 +42,10 @@ void Logger::LogErrorInFile(QString message)
     auto dateTime = QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss");
     if(file.open(QIODevice::WriteOnly | QIODevice::Append))
     {
+        QTextStream stream(&file);
         auto infoMessage = QString("ERROR [%1]: %2").arg(dateTime).arg(message);
         file.write(infoMessage.toStdString().c_str());
+        stream << infoMessage << '\n';
     }
     else
     {
