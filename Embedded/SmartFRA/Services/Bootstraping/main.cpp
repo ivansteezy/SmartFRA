f508fa934426da254b0794760b86729e476006d4
Syro <network.9961@gmail.com>
2022-10-05 19:18:53 -0500

    removing unused code

--- Embedded/SmartFRA/Services/Bootstraping/main.cpp
+++ Embedded/SmartFRA/Services/Bootstraping/main.cpp
@@ -4,16 +4,16 @@
 
 #include <opencv2/opencv.hpp>
 
-#include "../Logging/Logger.hpp"
-#include "../Networking/HttpRequestsManager.hpp"
+// #include "../Logging/Logger.hpp"
+// #include "../Networking/HttpRequestsManager.hpp"
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hola desde la version: " << CV_VERSION << " de OpenCV y con logger\n";
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
-    FRA::Networking::HttpRequestsManager<FRA::Logging::Logger> gNetworkManager;
-    gNetworkManager.Post(QString(), QByteArray());
+    // FRA::Networking::HttpRequestsManager<FRA::Logging::Logger> gNetworkManager;
+    // gNetworkManager.Post(QString(), QByteArray());
 
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
