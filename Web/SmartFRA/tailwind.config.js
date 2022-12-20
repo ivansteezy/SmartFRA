ad56fe6c1612a11bf2faed42e2f9c19e7c4e518d
Alex Hernandez <alma.hj0606@gmail.com>
2022-12-20 00:16:27 -0600

    Static Design Login-Admin
    Construction in code of the login view from figma

--- Web/SmartFRA/tailwind.config.js
+++ Web/SmartFRA/tailwind.config.js
@@ -6,8 +6,38 @@ module.exports = {
       fontFamily: {
         'hind-serif': 'Hind',
         'montse-serif': 'Montserrat'
-      }
+      },
+      backgroundImage: theme => ({
+        'background-login-large': "url('src/assets/img/wallpapers/login-wall-1-03.png')",
+        'background-login-small': "url('src/assets/img/wallpapers/smart-fra-ui-small-login-04.png')",
+      })
     },
+    colors: {
+      'primary-orange-color': '#F89A1D',
+      'secondary-blue-color': '#05364D',
+      'complementary-white-color': '#EEE9E4',
+      'complementary-gray-light-color': '#D9D9D9',
+      'white-pure-color': '#FFFFFF'
+    },
+    screens: {
+      'xsm': '200px',
+      // => @media (min-width: 200px) { ... }
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    textColor: {
+      'primary-orange': '#F89A1D',
+      'secondary-blue': '#05364D',
+      'complementary-white': '#EEE9E4',
+      'complementary-gray-light': '#D9D9D9',
+      'white-pure': '#FFFFFF'
+      }
   },
   plugins: [],
 }
