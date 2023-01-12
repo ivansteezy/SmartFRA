9bb0b6ef7429c363dfa4131301cb0c737c03ff5b
Alex Hernandez <alma.hj0606@gmail.com>
2023-01-11 22:20:33 -0600

    View Forgot-Password design, no backend

--- Web/SmartFRA/src/app/pages/forgot-password/forgot-password.component.ts
+++ Web/SmartFRA/src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { NavigationService } from 'src/app/services/common/navigation.service';
 import { NgToastService } from 'ng-angular-popup';
 import { CognitoService } from 'src/app/services/aws/cognito.service';
+import {AbstractControl, ValidatorFn} from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -15,6 +16,8 @@ export class ForgotPasswordComponent implements OnInit {
   myFormResetPassValidation: FormGroup;
 
   showPass = true;
+  showPass2 = true;
+  showModal=false;
 
   constructor(
     private navigation: NavigationService,
@@ -29,6 +32,8 @@ export class ForgotPasswordComponent implements OnInit {
 
     this.myFormResetPassValidation = this.fbV.group({
       code: ['', [Validators.required, Validators.pattern(/^[0-9]{6}$/)]],
+      pass: ['', [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]],
+      pass2: ['', [Validators.required, Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]]
     });
     
 
@@ -46,10 +51,26 @@ export class ForgotPasswordComponent implements OnInit {
       const rawFormValues = this.myFormResetPass.getRawValue();
       console.log(rawFormValues);
       this.toast.info({detail:"Codigo Enviado",summary:'Revisa tu correo, por favor.',duration:5000});
+      this.showModal = true;
     }
   }
 
-  public SubmitFormValidation() {}
+  public SubmitFormValidation() {
+
+    if(this.showModal){
+      if (this.myFormResetPassValidation.invalid) {
+        this.toast.error({detail:"Error en Informacion",summary:'Ingresa la informacion correcta',duration:5000});       
+      } else {
+        if(this.myFormResetPassValidation.getRawValue().pass != this.myFormResetPassValidation.getRawValue().pass2){
+          this.toast.error({detail:"Error en Informacion",summary:'Las contraseñas no coinciden',duration:5000}); 
+        }else{
+          console.log("im gonna work bitch");
+        }
+        
+      }
+    }
+  
+  }
 
   get f(): any {
     return this.myFormResetPass.controls;
@@ -58,6 +79,23 @@ export class ForgotPasswordComponent implements OnInit {
   get fV(): any {
     return this.myFormResetPassValidation.controls;
   }
+
+
+  get email() {
+    return this.myFormResetPass.get('email');
+  }
+  get code() {
+    return this.myFormResetPass.get('code');
+  }
+
+  get pass() {
+    return this.myFormResetPassValidation.get('pass');
+  }
+
+  get pass2() {
+    return this.myFormResetPassValidation.get('pass2');
+  }
+
 
   public NavigateToRegister() {
     this.navigation.NavigateToRoute('register');
@@ -70,4 +108,10 @@ export class ForgotPasswordComponent implements OnInit {
   public NavigateToLogin() {
     this.navigation.NavigateToRoute('login');
   }
+
+  
+
 }
+
+
+
