05a63cfffecef22b922be42b3c7909d4375cd5c0
Ivan Ayala <network.9961@gmail.com>
2023-01-21 00:43:30 -0600

    change password support

--- Web/SmartFRA/src/app/pages/forgot-password/forgot-password.component.ts
+++ Web/SmartFRA/src/app/pages/forgot-password/forgot-password.component.ts
@@ -3,8 +3,9 @@ import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/f
 import { FormBuilder } from '@angular/forms';
 import { NavigationService } from 'src/app/services/common/navigation.service';
 import { NgToastService } from 'ng-angular-popup';
-import { CognitoService } from 'src/app/services/aws/cognito.service';
+import { CognitoService, RestorePasswordData } from 'src/app/services/aws/cognito.service';
 import {AbstractControl, ValidatorFn} from '@angular/forms';
+import { RestorePasswordService } from 'src/app/services/aws/restore-password.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -24,7 +25,8 @@ export class ForgotPasswordComponent implements OnInit {
     private fb: FormBuilder,
     private fbV: FormBuilder,
     private toast: NgToastService,
-    private cognitoService: CognitoService
+    private cognitoService: CognitoService,
+    private restorePassword: RestorePasswordService
   ) {
     this.myFormResetPass = this.fb.group({
       email: ['', [Validators.required, Validators.pattern(/^\S+@\S+\.\S+$/)]]
@@ -49,27 +51,54 @@ export class ForgotPasswordComponent implements OnInit {
 
     } else {
       const rawFormValues = this.myFormResetPass.getRawValue();
-      console.log(rawFormValues);
-      this.toast.info({detail:"Codigo Enviado",summary:'Revisa tu correo, por favor.',duration:5000});
-      this.showModal = true;
+      this.SendVerificationCode(rawFormValues.email);
     }
   }
 
-  public SubmitFormValidation() {
+  SendVerificationCode(email: string) {
+    this.restorePassword.SendRestorePasswordCode(email).then(result => {
+      this.toast.info({detail:"Codigo Enviado",summary:'Revisa tu correo, por favor.',duration:5000});
+      this.showModal = true;
+    }).catch(error => {
+      console.error('Error trying to send the verification code: ' + error);
+      this.toast.error({detail:"Error al enviar su codigo",summary:'Por favor, intente mas tarde.',duration:5000})
+    });
+  }
 
-    if(this.showModal){
-      if (this.myFormResetPassValidation.invalid) {
-        this.toast.error({detail:"Error en Informacion",summary:'Ingresa la informacion correcta',duration:5000});       
-      } else {
-        if(this.myFormResetPassValidation.getRawValue().pass != this.myFormResetPassValidation.getRawValue().pass2){
-          this.toast.error({detail:"Error en Informacion",summary:'Las contraseñas no coinciden',duration:5000}); 
-        }else{
-          console.log("im gonna work bitch");
-        }
-        
-      }
+  ChangePassword(email: string, verificationCode: string, newPassword: string) {
+    this.restorePassword.VerifyRestorePasswordCode({ email: email, verificationCode: verificationCode, password: newPassword }).then(result => {
+      this.navigation.NavigateToRoute('login');
+      this.toast.success({detail:"Contraseña actualizada",summary:'Su contraseña ha sido correctamente actualizada.',duration:5000});
+    }).catch(error => {
+      console.error('Error trying to update password ' + error);
+      this.toast.error({detail:"Error al actualizar su contraseña",summary:'Por favor, intente mas tarde.',duration:5000});
+    });
+  }
+
+  public SubmitFormValidation() {
+    const email = this.myFormResetPass.getRawValue().email;
+    const verificationCode = this.myFormResetPassValidation.getRawValue().code;
+    const password = this.myFormResetPassValidation.getRawValue().pass;
+
+    if(!this.showModal)
+    {
+      return;
     }
-  
+
+    if(this.myFormResetPassValidation.invalid)
+    {
+      this.toast.error({detail:"Error en Informacion",summary:'Ingresa la informacion correcta',duration:5000});
+      return;
+    }
+
+    const isSamePassword = this.myFormResetPassValidation.getRawValue().pass == this.myFormResetPassValidation.getRawValue().pass2;
+    if(!isSamePassword)
+    {
+      this.toast.error({detail:"Error en Informacion",summary:'Las contraseñas no coinciden',duration:5000});
+      return;
+    }
+
+    this.ChangePassword(email, verificationCode, password);
   }
 
   get f(): any {
