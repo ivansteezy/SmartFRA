db448419773757e34929f40e93b3f8561c97bb2d
Hector <a19310176@live.ceti.mx>
2023-02-24 22:49:29 -0600

    Working DataAccess Resident Access

--- DataAccess/Local/index.js
+++ DataAccess/Local/index.js
@@ -1,11 +1,12 @@
 const resident = require('./Controller/ResidentController');
 const residentAccess = require('./Controller/ResidentAccessController');
-const express = require("express");
-const app = express();
 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
 app.listen(3000, () => console.log("Smart FRA server is now running on port 3000..."));
+app.use(cors());
 app.use(express.json());
 app.use('/resident-access', residentAccess);
 app.use('/resident', resident);
-
-
--- Web/SmartFRA/src/app/services/managers/resident-access-manager.service.ts
+++ Web/SmartFRA/src/app/services/managers/resident-access-manager.service.ts
@@ -10,7 +10,7 @@ import { ResidentAccessEndpoints } from '../../data-access/resident-access.endpo
 export class ResidentManagerService {
   constructor(private http: HttpRequestsService) { }
 
-  public GetAllResidentAccess(name: string) : Observable<Array<ResidentAccess>> {
+  public GetAllResidentAccess() : Observable<Array<ResidentAccess>> {
     // call http requests service for a GET for the resident by id endpoint
     return this.http.Get<ResidentAccess>(ResidentAccessEndpoints.GetAllResidentAccess);
   }
