2b43e717542fbe0572987ea7051c171d227779a8
Alex Hernandez <alma.hj0606@gmail.com>
2023-06-22 00:04:41 -0600

    Adding Base to UserSession Management
    Base to get data from the user and keep it inside the sessionStorage

--- Web/SmartFRA/src/app/pages/dashboard/dashboard.component.ts
+++ Web/SmartFRA/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationService } from 'src/app/services/common/navigation.service';
 import { menuBarData } from './menu-bar-data';
+import { CognitoService } from 'src/app/services/aws/cognito.service';
+import { NgToastService } from 'ng-angular-popup';
+import { HttpRequestsService } from 'src/app/services/common/http-requests.service';
+import { catchError, tap } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,12 +19,40 @@ export class DashboardComponent implements OnInit {
   navData = menuBarData;
   currentContent : string = "";
 
-  constructor(private navigationService: NavigationService) {
+  constructor(private navigationService: NavigationService, private toast: NgToastService,private cognitoService: CognitoService, private http: HttpRequestsService) {
+
+    let apiUrl = "http://localhost:3000/resident/ResidentByEmail/"
+    this.cognitoService.GetLoggedUser().then((res:any) => {
+      console.log(res.payload.email);
+
+      this.http.Get(apiUrl, `%27${res.payload.email}%27` )
+      .pipe(
+        tap((response) => {
+         console.log("He guardado la data");
+         if(JSON.stringify(response) === JSON.stringify(JSON.parse("[]"))){
+          this.navigationService.NavigateToRoute('login');
+         }else{
+          sessionStorage.setItem('apiResponse', JSON.stringify(response));
+         }
+         
+        }),
+        catchError((error) => {
+          console.log("No he guardado la data");
+          throw error;
+        })
+      )
+      .subscribe();
+
+    }).catch((error) => {
+      console.log("Oh algo ha salido mal");
+      console.log(error);
+    })
 
    }
 
   ngOnInit(): void {
   }
+  
 
   public NavigateToLogin() {
     this.navigationService.NavigateToRoute('login');
--- Web/SmartFRA/src/app/pages/login/login.component.ts
+++ Web/SmartFRA/src/app/pages/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder } from '@angular/forms';
 import { NavigationService } from 'src/app/services/common/navigation.service';
 import { NgToastService } from 'ng-angular-popup';
 import { CognitoService } from 'src/app/services/aws/cognito.service';
+import { catchError, tap } from 'rxjs';
+import { HttpRequestsService } from 'src/app/services/common/http-requests.service';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +18,8 @@ export class LoginComponent implements OnInit {
   constructor(private navigation: NavigationService, 
     private fb: FormBuilder, 
     private toast: NgToastService,
-    private cognitoService: CognitoService) {
+    private cognitoService: CognitoService,
+    private http: HttpRequestsService) {
       
     this.myForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern(/^\S+@\S+\.\S+$/)]],
@@ -33,13 +36,35 @@ export class LoginComponent implements OnInit {
       password: this.myForm.getRawValue().password
     }
 
+
+    const apiUrl = `http://localhost:3000/resident/ResidentByEmail/${data['email']}`;
+
     this.cognitoService.AuthenticateUser(data).then(res => {
-      this.toast.success({detail:"Ingreso correcto",summary:'Bienvenido a SmartFRA',duration:5000});
-      this.navigation.NavigateToRoute('dashboard');
+      console.log("Authentication good AWS");
+      console.log(res);
+      /* search in database*/
+
+      // this.http.Get<any>(apiUrl)
+      // .subscribe(
+      //   response => {
+      //     const apiResponse = response;
+      //     console.log(apiResponse);
+      //   },
+      //   error => {
+      //     console.log(error);
+      //   }
+      // );
+
+          this.toast.success({detail:"Ingreso correcto",summary:'Bienvenido a SmartFRA',duration:5000});
+          //relocation
+          this.navigation.NavigateToRoute('dashboard');
+      
     }).catch(error => {
       this.toast.error({detail:"Error de Inicio de Sesión",summary:'Usuario o contraseña incorrectos.',duration:5000});
     })
   }
+
+
 
   public SubmitForm() {
     if (this.myForm.invalid) {
@@ -68,4 +93,7 @@ export class LoginComponent implements OnInit {
   public NavigateToResetPass() {
     this.navigation.NavigateToRoute('forgot-password');
   }
+
+  
+  
 }
